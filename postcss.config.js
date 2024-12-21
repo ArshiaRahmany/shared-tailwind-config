5ba284773ce4a8b3a2555908f40ae45ee9d6fa41
ArshiaRahmany <rahmanyarshia@gmail.com>
2024-12-21 16:43:59 +0330

    added : all button colors

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    plugins: {
+      tailwindcss: {},    // Adds Tailwind CSS processing
+      autoprefixer: {},   // Adds vendor prefixes for better cross-browser compatibility
+    },
+  };
+  
